427180e13966c9f42aedf2a6cddb975117a58b4b
Nick Cooper <cooper.b.nicholas@gmail.com>
2020-09-28 21:07:22 -0500

    improvements to the switch

--- custom_components/hautomate/switch.py
+++ custom_components/hautomate/switch.py
@@ -2,18 +2,12 @@ import logging
 
 from homeassistant.helpers.entity import ToggleEntity
 from hautomate.enums import IntentState
-from .const import DOMAIN
 
 
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    hauto = hass.data[DOMAIN]
-    _LOGGER.info(f'(switch) uhhh was it this easy? hauto={hauto}')
-
-
-class HautoIntentEntity(ToggleEntity):
+class HautoIntent(ToggleEntity):
     """
     """
 
@@ -57,16 +51,18 @@ class HautoIntentEntity(ToggleEntity):
         """ Return the name of the entity. """
         return f'intent_{self.hauto_intent._id}'
 
-    @property
-    def state(self) -> str:
-        """ Return the state of the entity. """
-        return self.hauto_intent._state.value
+    # @property
+    # def state(self) -> str:
+    #     """ Return the state of the entity. """
+    #     return self.hauto_intent._state.value.lower()
 
     @property
     def state_attributes(self) -> str:
         """ Return the data of the entity. """
         data = {
-            # 'parent': self.hauto_intent._app.name,
+            'func': getattr(self.hauto_intent.func, '__name__', 'undefined'),
+            'concurrency': self.hauto_intent.concurrency,
+            'parent': getattr(self.hauto_intent._app, 'name', None),
             'event': self.hauto_intent.event,
             'last_ran': self.hauto_intent.last_ran,
             'runs': self.hauto_intent.runs,
@@ -84,7 +80,7 @@ class HautoIntentEntity(ToggleEntity):
     @property
     def is_on(self) -> bool:
         """ Return True if entity is on. """
-        raise self.hauto_intent._state not in (IntentState.paused, IntentState.cancelled)
+        return self.hauto_intent._state == IntentState.ready
 
     # ...
 
@@ -97,3 +93,10 @@ class HautoIntentEntity(ToggleEntity):
         """
         """
         await self._pause()
+
+    async def async_toggle(self, **kwargs) -> None:
+        """Toggle the entity."""
+        if self.is_on:
+            await self.async_turn_off(**kwargs)
+        else:
+            await self.async_turn_on(**kwargs)
